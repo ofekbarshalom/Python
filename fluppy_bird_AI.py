3cc8bac2be1787cb270ebb27f02b1e51db96f28f
ofekbarshalom <155369472+ofekbarshalom@users.noreply.github.com>
2024-10-08 09:35:10 +0300

    Update fluppy_bird_AI.py

--- fluppy_bird_AI.py
+++ fluppy_bird_AI.py
@@ -8,6 +8,8 @@ pygame.font.init()
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
+GEN = 0
+
 BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),
              pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
              pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
@@ -153,13 +155,17 @@ class Base:
         win.blit(self.IMG, (self.x1, self.y))
         win.blit(self.IMG, (self.x2, self.y))
 
-def draw_window(win, birds, pipes, base, score):
+def draw_window(win, birds, pipes, base, score, gen):
     win.blit(BG_IMG, (0, 0))
     for pipe in pipes:
         pipe.draw(win)
 
     text = STAT_FONT.render("Score: " + str(score), 1, (255,255,255))
     win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
+
+    text = STAT_FONT.render("Gen: " + str(gen), 1, (255, 255, 255))
+    win.blit(text, (10 , 10))
+
     base.draw(win)
     for bird in birds:
         bird.draw(win)
@@ -168,6 +174,8 @@ def draw_window(win, birds, pipes, base, score):
 
 
 def main(genomes, config):
+    global GEN
+    GEN += 1
     nets = []
     ge =[]
     birds = []
@@ -246,8 +254,11 @@ def main(genomes, config):
                 nets.pop(x)
                 ge.pop(x)
 
+        if score > 100:
+            break
+
         base.move()
-        draw_window(win, birds, pipes, base, score)
+        draw_window(win, birds, pipes, base, score, GEN)
 
 
 def run(config_path):
