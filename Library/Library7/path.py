af6b8c21495713fc4d9b768c07f61b8940631d81
Ofek Bar shalom <155369472+ofekbarshalom@users.noreply.github.com>
2025-01-09 00:22:12 +0200

    Add files via upload

---
+++ Library/Library7/path.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class Paths(Enum):
+    USERS = "files/users.csv"
+    BOOKS = "files/books.csv"
+    AVAILABLE_BOOKS = "files/available_books.csv"
+    LOANED_BOOKS = "files/loaned_books.csv"
